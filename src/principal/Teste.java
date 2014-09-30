fce742941f8c573de831b500779dbd07c8f60596
allexllima <allex.lima@hotmail.com>
2014-09-30 19:26:14 -0300

    Alteração do método soma

--- src/principal/Teste.java
+++ src/principal/Teste.java
@@ -6,12 +6,10 @@ public class Teste {
 		// TODO Auto-generated method stub
 
 	}
-public static int soma(int a, int b){
-	return a+b;
-	
-	
-	
+public static int soma(int a, int b, int c) {
+	return a+b+c;
 }
+
 public static int sub(int a, int b){
 	return a-b;
 	
