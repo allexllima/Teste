e87d9eaac84fc3220dc652482f4a23e73971e7ad
arnaldosales <arnaldohghp@gmail.com>
2014-09-30 19:27:03 -0300

    implementação da multiplicação

--- src/principal/Teste.java
+++ src/principal/Teste.java
@@ -10,10 +10,16 @@ public static int soma(int a, int b){
 	return a+b;
 	
 	
-	
+	//testando o git
 }
 public static int sub(int a, int b){
 	return a-b;
+	
+	
+	
+}
+public static int multiplicacao(int a, int b){
+	return a*b;
 	
 	
 	
